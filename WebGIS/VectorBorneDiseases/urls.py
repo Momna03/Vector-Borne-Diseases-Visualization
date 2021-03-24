baad2e51e2800db511ce67a44048ce7d8cc04ded
Momna03 <momina.javaid03@gmail.com>
2021-03-24 13:43:38 +0500

    Add fourth template dashboard4

--- WebGIS/VectorBorneDiseases/urls.py
+++ WebGIS/VectorBorneDiseases/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('dashboard/1/', views.dashboard1, name='dashboard1'),
     path('dashboard/2/', views.dashboard2, name='dashboard2'),
     path('dashboard/3/', views.dashboard3, name='dashboard3'),
+    path('dashboard/4/', views.dashboard4, name='dashboard4'),
 ]
--- WebGIS/VectorBorneDiseases/views.py
+++ WebGIS/VectorBorneDiseases/views.py
@@ -3,20 +3,19 @@ from WebGIS import settings
 
 # Create your views here.
 
+api_key = settings.GOOGLE_MAPS_API_KEY
 def dashboard1(request):
-    
-    api_key = settings.GOOGLE_MAPS_API_KEY
-    
+
     return render(request, 'dashboard1.html', {'api_key': api_key})
 
 def dashboard2(request):
     
-    api_key = settings.GOOGLE_MAPS_API_KEY
-    
     return render(request, 'dashboard2.html', {'api_key': api_key})
 
 def dashboard3(request):
-    
-    api_key = settings.GOOGLE_MAPS_API_KEY
-    
+
     return render(request, 'dashboard3.html', {'api_key': api_key})
+
+def dashboard4(request):
+
+    return render(request, 'dashboard4.html',  {'api_key': api_key})
